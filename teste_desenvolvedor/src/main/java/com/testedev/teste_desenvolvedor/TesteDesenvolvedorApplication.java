c1512584d8714b5998684a587293f300cdb79812
William Pagani <williamlg_pagani@hotmail.com>
2022-11-23 16:47:51 -0300

    primeiro commit

---
+++ teste_desenvolvedor/src/main/java/com/testedev/teste_desenvolvedor/TesteDesenvolvedorApplication.java
@@ -0,0 +1,13 @@
+package com.testedev.teste_desenvolvedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDesenvolvedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDesenvolvedorApplication.class, args);
+	}
+
+}
---
+++ teste_desenvolvedor/src/test/java/com/testedev/teste_desenvolvedor/TesteDesenvolvedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testedev.teste_desenvolvedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDesenvolvedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
